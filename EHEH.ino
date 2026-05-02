fd4868c939257c90e72f5824e745cd5d03b215f5
beanzzuh <sabeanie13bean@gmail.com>
2026-05-02 10:55:20 -0700

    Update EHEH.ino

--- EHEH.ino
+++ EHEH.ino
@@ -1,6 +1,9 @@
 #include "SevSeg.h"
 #define IRSbPin A1 //is time
 #define setEHEH A2 //set alrarm
+#include <CronAlarms.h>
+#include <TimeLib.h>
+
 SevSeg sevseg; 
 //Segment Pins below
 int aPin = 2;
@@ -23,8 +26,9 @@ int lowC = 0;
 
 bool settingAlarm = false;
 bool alarmArmed = false;
-unsigned long currentTime = 0;
+unsigned long currentTime = 360000; //og 360000
 unsigned long alarmTime = -1;
+
 
 int clockMode = 0; // 0 = normal clock 1 = setting clock
 
@@ -33,30 +37,15 @@ int alarmEH = 432;
  
 int buttonState = 0;
 
+int hours;
+int minutes;
 
-/*remote stuff
-power = BA45FF00 (vol up)
-VOL+ = B946FF00 (vol down)
-FUNC/STOP = B847FF00 (dont stop add more time)
-ARROWSL = BB44FF00 (increse pitch)
-PLAYPUASE = BF40FF00 (no)
-ARROWSR = BC43FF00 (incres potch)
-ARROWD = F807FF00 (incres po\itch)
-VOL- = EA15FF00 (vol up)
-ARROWU = F609FF00(lowerpitch)
-0 = E916FF00
-eq = E619FF00 (test alarm )
-T/REPT = F20DFF00
-1= F30CFF00
-2= E718FF00
-3=A15EFF00
-4=F708FF00
-5=E31CFF00
-6=A55AFF00
-7=BD42FF00
-8=AD52FF00
-9 = B54AFF00
-*/
+
+
+time_t t = now();
+setTime(t);
+
+
 
 void blare(unsigned EHEH = 432, unsigned stopEHEH = 1000){
   //the alarm eh part use EQ
@@ -73,7 +62,11 @@ void setup() {
   byte digitPin[] = {7,8,9,10};
   byte segmentPins[] = {2,3,4,5,11,12,13,6};
   bool ROS = 0; //reisitor onsegnemet
-  sevseg.begin(COMMON_CATHODE,numDigits,digitPin,segmentPins,ROS);
+  bool LZ = true; //LEading zero
+  bool disableDecimalPoint = true;
+  bool updateWitDelays = false;
+  sevseg.begin(COMMON_CATHODE,numDigits,digitPin,segmentPins,ROS,updateWitDelays,
+  LZ, disableDecimalPoint);
   sevseg.setBrightness(90);
 
   pinMode(buzzerPin, OUTPUT);
@@ -82,13 +75,20 @@ void setup() {
  
 }
 
+
+
 void loop() {
-  sevseg.setNumber(currentTime, 2);
+  
+  //int time = makeTime(currentTime);
+  if(time<9){
+    sevseg.setSegmentsDigit(2, 0x63);
+  }
+  sevseg.setNumber(minute(t));
   sevseg.refreshDisplay();
   buttonState = digitalRead(IRSbPin);
     if (buttonState == HIGH){
     highC++;
-     
+    //makeTime(currentTime);
     lowC = 0;
   }
   else{
@@ -100,7 +100,23 @@ void loop() {
     highC = 0;
   }
   currentTime += millis() - currentTime;
-  Serial.println(currentTime);
+  //Serial.println(currentTime);
 
     // delay(1000);
 }
+
+
+
+// int makeTime(long lilSecs){
+//   int hours;
+//   int minutes;
+//   hours = lilSecs/36000000; //og 36000000
+//   lilSecs -= hours*36000000; //og 36000000
+//   minutes = lilSecs/60000; //og 60000
+//   int finalDisplay = hours*100+minutes;
+//   Serial.println(finalDisplay);
+//   Serial.println("hours" + hours);
+//   Serial.println("minutes" + minutes);
+//   //Serial.println(minutes);
+//   return finalDisplay;
+// }
